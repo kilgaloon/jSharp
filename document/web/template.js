55b4f4c77492c4dda2e304a69da948ab3c579e0b
Strahinja <radju22@gmail.com>
2014-10-24 16:28:13 +0200

    Create template.js

---
+++ document/web/template.js
@@ -0,0 +1,50 @@
+
+var template = [];
+
+// template object
+if(window.Ravel.isLoaded) {
+
+
+	var _TEMPLATE = function( url ) {
+
+			this.url = url;
+
+			this.load = function( params ) {
+
+				if (window.XMLHttpRequest) { ajax = new XMLHttpRequest() }
+
+				if(typeof(params) == "object" ) {
+
+					p = JSON.stringify(params).replace(/[\"\}\{]/g, '');
+					param = p.replace(/[\:]/g,"=").replace(/[\,]/g, "&");
+					
+					ajax.open("GET", "document/web/template/" + this.url + "?" + param, false);
+					ajax.setRequestHeader("Content-Type", "text/html; charset=UTF-8");
+					ajax.send(null);
+					ajax_response = ajax.responseText;
+
+					return ajax_response;
+
+			
+				} else {
+
+					ajax.open("GET", "document/web/template/" + this.url, false);
+					ajax.setRequestHeader("Content-Type", "text/html; charset=UTF-8");
+					ajax.send();
+					ajax_response = ajax.responseText;
+
+					return ajax_response;
+
+				}		
+
+			}
+
+
+			return this;
+
+	}
+
+
+
+
+}
