3f9ec9fe8063745c41201e64fa32855682ba2838
Kilgaloon <radju22@gmail.com>
2014-11-24 12:26:33 +0100

    ensuring to set custom path to template

--- document/web/template.js
+++ document/web/template.js
@@ -18,7 +18,7 @@ core.packageReady();
 					p = JSON.stringify(params).replace(/[\"\}\{]/g, '');
 					param = p.replace(/[\:]/g,"=").replace(/[\,]/g, "&");
 					
-					ajax.open("GET", "document/web/template/" + this.url + "?" + param, false);
+					ajax.open("GET", this.url + "?" + param, false);
 					ajax.setRequestHeader("Content-Type", "text/html; charset=UTF-8");
 					ajax.send(null);
 					ajax_response = ajax.responseText;
@@ -28,7 +28,7 @@ core.packageReady();
 			
 				} else {
 
-					ajax.open("GET", "document/web/template/" + this.url, false);
+					ajax.open("GET", this.url, false);
 					ajax.setRequestHeader("Content-Type", "text/html; charset=UTF-8");
 					ajax.send();
 					ajax_response = ajax.responseText;
