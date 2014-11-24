cab7992566c56d47be2b11a9153a25075b0e6bb3
Kilgaloon <radju22@gmail.com>
2014-11-24 20:23:02 +0100

    Completing all events

--- document/web/events.js
+++ document/web/events.js
@@ -26,15 +26,75 @@ plug.onbeforeupload = function( foo ) {
 
 }
 
-plug.onblur = function() {}
+plug.onblur = function( foo )  {
 
-plug.oncancel = function() {}
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onblur = foo;
+	} else {
 
-plug.oncanplay = function() {}
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onblur = foo;
+		}
 
-plug.oncanplaythrough = function() {}
+	}
 
-plug.onchange = function() {}
+}
+
+plug.oncancel = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.oncancel = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].oncancel = foo;
+		}
+
+	}
+
+}
+
+plug.oncanplay = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.oncanplay = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].oncanplay = foo;
+		}
+
+	}
+
+}
+
+plug.oncanplaythrough = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.oncanplaythrough = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].oncanplaythrough = foo;
+		}
+
+	}
+
+}
+
+plug.onchange = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onchange = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onchange = foo;
+		}
+
+	}
+
+}
 
 plug.onclick = function( foo ) {
 
@@ -49,53 +109,341 @@ plug.onclick = function( foo ) {
 	}
 }
 
-plug.onclose = function() {}
+plug.onclose = function( foo )  {
 
-plug.oncontextmenu = function() {}
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onclose = foo;
+	} else {
 
-plug.oncuechange = function() {}
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onclose = foo;
+		}
 
-plug.ondblclick = function() {}
+	}
 
-plug.ondevicemotion = function() {}
+}
 
-plug.ondeviceoriantation = function() {}
+plug.onrclick = function( foo )  {
 
-plug.ondrag = function() {}
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.oncontextmenu = foo;
+	} else {
 
-plug.ondragend = function() {}
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].oncontextmenu = foo;
+		}
 
-plug.ondragenter = function() {}
+	}
 
-plug.ondragleave = function() {}
+}
 
-plug.ondragover = function() {}
+plug.oncuechange = function( foo )  {
 
-plug.ondragstart = function() {}
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.oncuechange = foo;
+	} else {
 
-plug.ondrop = function() {}
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].oncuechange = foo;
+		}
 
-plug.ondurationchange = function() {}
+	}
 
-plug.onemptied = function() {}
+}
 
-plug.onended = function() {}
+plug.ondblclick = function( foo )  {
 
-plug.onerror = function() {}
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.ondblclick = foo;
+	} else {
 
-plug.onfocus = function() {}
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].ondblclick = foo;
+		}
 
-plug.onhashchange = function() {}
+	}
 
-plug.oninput = function() {}
+}
 
-plug.oninvalid = function() {}
+plug.ondevicemotion = function( foo )  {
 
-plug.onkeydown = function() {}
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.ondevicemotion = foo;
+	} else {
 
-plug.onkeypress = function() {}
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].ondevicemotion = foo;
+		}
 
-plug.onkeyup = function() {}
+	}
+
+}
+
+plug.ondeviceoriantation = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.ondeviceoriantation = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].ondeviceoriantation = foo;
+		}
+
+	}
+
+}
+
+plug.ondrag = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.ondrag = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].ondrag = foo;
+		}
+
+	}
+
+}
+
+plug.ondragend = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.ondragend = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].ondragend = foo;
+		}
+
+	}
+
+}
+
+plug.ondragenter = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.ondragenter = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].ondragenter = foo;
+		}
+
+	}
+
+}
+
+plug.ondragleave = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.ondragleave = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].ondragleave = foo;
+		}
+
+	}
+
+}
+
+plug.ondragover = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.ondragover = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].ondragover = foo;
+		}
+
+	}
+
+}
+
+plug.ondragstart = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.ondragstart = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].ondragstart = foo;
+		}
+
+	}
+
+}
+
+plug.ondrop = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.ondrop = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].ondrop = foo;
+		}
+
+	}
+
+}
+
+plug.ondurationchange = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.ondurationchange = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].ondurationchange = foo;
+		}
+
+	}
+
+}
+
+plug.onemptied = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onemptied = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onemptied = foo;
+		}
+
+	}
+
+}
+
+plug.onended = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onended = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onended = foo;
+		}
+
+	}
+
+}
+
+plug.onerror = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onerror = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onerror = foo;
+		}
+
+	}
+
+}
+
+plug.onfocus = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onfocus = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onfocus = foo;
+		}
+
+	}
+
+}
+
+plug.onhashchange = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onhashchange = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onhashchange = foo;
+		}
+
+	}
+
+}
+
+plug.oninput = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.oninput = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].oninput = foo;
+		}
+
+	}
+
+}
+
+plug.oninvalid = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.oninvalid = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].oninvalid = foo;
+		}
+
+	}
+
+}
+
+plug.onkeydown = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onkeydown = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onkeydown = foo;
+		}
+
+	}
+
+}
+
+plug.onkeypress = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onkeypress = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onkeypress = foo;
+		}
+
+	}
+
+}
+
+plug.onkeyup = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onkeyup = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onkeyup = foo;
+		}
+
+	}
+
+}
 
 plug.onload = function( foo ) {
 
@@ -111,13 +459,61 @@ plug.onload = function( foo ) {
 
 }
 
-plug.onloadeddata = function() {}
+plug.onloadeddata = function( foo )  {
 
-plug.onloadedmetadata = function() {}
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onloadeddata = foo;
+	} else {
 
-plug.onloadstart = function() {}
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onloadeddata = foo;
+		}
 
-plug.onmessage = function() {}
+	}
+
+}
+
+plug.onloadedmetadata = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onloadedmetadata = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onloadedmetadata = foo;
+		}
+
+	}
+
+}
+
+plug.onloadstart = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onloadstart = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onloadstart = foo;
+		}
+
+	}
+
+}
+
+plug.onmessage = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onmessage = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onmessage = foo;
+		}
+
+	}
+
+}
 
 plug.onmousedown = function( foo ) {
 
@@ -175,7 +571,7 @@ plug.onmousemove = function( foo ) {
 
 }
 
-plug.onmouseout = function() {
+plug.onmouseout = function( foo )  {
 
 	if(typeof object_in_buffer.length == "undefined") {
 		object_in_buffer.onmouseout = foo;
@@ -235,7 +631,7 @@ plug.onwheelup = function( foo ) {
 
 	if(typeof object_in_buffer.length == "undefined") {
 
-		object_in_buffer.onmousewheel = function() {
+		object_in_buffer.onmousewheel = function( foo )  {
 
 			if(event.wheelDelta < 0) {
 				foo();
@@ -246,7 +642,7 @@ plug.onwheelup = function( foo ) {
 	} else {
 
 		for(i = 0; i < object_in_buffer.length; i++) {
-			object_in_buffer[i].onmousewheel = function() {
+			object_in_buffer[i].onmousewheel = function( foo )  {
 
 				if(event.wheelDelta < 0) {
 					foo();
@@ -263,7 +659,7 @@ plug.onwheeldown = function( foo ) {
 
 	if(typeof object_in_buffer.length == "undefined") {
 
-		object_in_buffer.onmousewheel = function() {
+		object_in_buffer.onmousewheel = function( foo )  {
 
 			if(event.wheelDelta > 0) {
 				foo();
@@ -274,7 +670,7 @@ plug.onwheeldown = function( foo ) {
 	} else {
 
 		for(i = 0; i < object_in_buffer.length; i++) {
-			object_in_buffer[i].onmousewheel = function() {
+			object_in_buffer[i].onmousewheel = function( foo )  {
 
 				if(event.wheelDelta > 0) {
 					foo();
@@ -287,21 +683,117 @@ plug.onwheeldown = function( foo ) {
 
 }
 
-plug.onoffline = function() {}
+plug.onoffline = function( foo )  {
 
-plug.ononline = function() {}
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onoffline = foo;
+	} else {
 
-plug.onpagehide = function() {}
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onoffline = foo;
+		}
 
-plug.onpageshow = function() {}
+	}
 
-plug.onpause = function() {}
+}
 
-plug.onplay = function() {}
+plug.ononline = function( foo )  {
 
-plug.onplaying = function() {}
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.ononline = foo;
+	} else {
 
-plug.onpopstate = function() {}
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].ononline = foo;
+		}
+
+	}
+
+}
+
+plug.onpagehide = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onpagehide = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onpagehide = foo;
+		}
+
+	}
+
+}
+
+plug.onpageshow = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onpageshow = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onpageshow = foo;
+		}
+
+	}
+
+}
+
+plug.onpause = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onpause = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onpause = foo;
+		}
+
+	}
+
+}
+
+plug.onplay = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onplay = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onplay = foo;
+		}
+
+	}
+
+}
+
+plug.onplaying = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onplaying = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onplaying = foo;
+		}
+
+	}
+
+}
+
+plug.onpopstate = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onpopstate = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onpopstate = foo;
+		}
+
+	}
+
+}
 
 plug.onprogress = function( foo ) {
 
@@ -317,43 +809,271 @@ plug.onprogress = function( foo ) {
 
 }
 
-plug.onratechange = function() {}
+plug.onratechange = function( foo )  {
 
-plug.onreset = function() {}
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onratechange = foo;
+	} else {
 
-plug.onresize = function() {}
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onratechange = foo;
+		}
 
-plug.onscroll = function() {}
+	}
 
-plug.onsearch = function() {}
+}
 
-plug.onseeked = function() {}
+plug.onreset = function( foo )  {
 
-plug.onseeking = function() {}
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onreset = foo;
+	} else {
 
-plug.onselect = function() {}
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onreset = foo;
+		}
 
-plug.onshow = function() {}
+	}
 
-plug.onstalled = function() {}
+}
 
-plug.onstorage = function() {}
+plug.onresize = function( foo )  {
 
-plug.onsubmit = function() {}
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onresize = foo;
+	} else {
 
-plug.onsuspend = function() {}
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onresize = foo;
+		}
 
-plug.ontimeupdate = function() {}
+	}
 
-plug.ontransitionend = function() {}
+}
 
-plug.onunload = function() {}
+plug.onscroll = function( foo )  {
 
-plug.onvolumechange = function() {}
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onscroll = foo;
+	} else {
 
-plug.onwaiting = function() {}
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onscroll = foo;
+		}
 
-plug.onwheel = function() {}
+	}
+
+}
+
+plug.onsearch = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onsearch = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onsearch = foo;
+		}
+
+	}
+
+}
+
+plug.onseeked = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onseeked = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onseeked = foo;
+		}
+
+	}
+
+}
+
+plug.onseeking = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onseeking = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onseeking = foo;
+		}
+
+	}
+
+}
+
+plug.onselect = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onselect = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onselect = foo;
+		}
+
+	}
+
+}
+
+plug.onshow = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onshow = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onshow = foo;
+		}
+
+	}
+
+}
+
+plug.onstalled = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onstalled = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onstalled = foo;
+		}
+
+	}
+
+}
+
+plug.onstorage = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onstorage = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onstorage = foo;
+		}
+
+	}
+
+}
+
+plug.onsubmit = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onsubmit = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onsubmit = foo;
+		}
+
+	}
+
+}
+
+plug.onsuspend = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onsuspend = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onsuspend = foo;
+		}
+
+	}
+
+}
+
+plug.ontimeupdate = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.ontimeupdate = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].ontimeupdate = foo;
+		}
+
+	}
+
+}
+
+plug.ontransitionend = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.ontransitionend = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].ontransitionend = foo;
+		}
+
+	}
+
+}
+
+plug.onunload = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onunload = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onunload = foo;
+		}
+
+	}
+
+}
+
+plug.onvolumechange = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onvolumechange = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onvolumechange = foo;
+		}
+
+	}
+
+}
+
+plug.onwaiting = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onwaiting = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onwaiting = foo;
+		}
+
+	}
+
+}
+
+plug.onwheel = function( foo )  {
+
+	if(typeof object_in_buffer.length == "undefined") {
+		object_in_buffer.onwheel = foo;
+	} else {
+
+		for(i = 0; i < object_in_buffer.length; i++) {
+			object_in_buffer[i].onwheel = foo;
+		}
+
+	}
+
+}
 
 plug.listen = function( listener, foo ) {	
 	if(typeof object_in_buffer.length == "undefined") {
